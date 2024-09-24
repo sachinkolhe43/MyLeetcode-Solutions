55570cf6a8628a377a6756118348190d695c9828
Sachin kolhe <59631492+sachinkolhe43@users.noreply.github.com>
2024-09-24 11:59:35 +0530

    Create 53. Maximum Subarray.java

---
+++ 53. Maximum Subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int total = 0;
+        for (int n : nums) {
+            if (total < 0) {
+                total = 0;
+            }
+            total += n;
+            res = Math.max(res, total);
+        }
+        return res;      
+    }
+}
+
+/*
+ * In this question we calculate the max SubArray
+ * 
+ */
